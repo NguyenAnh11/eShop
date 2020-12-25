8b515e59b89562fba431f8f187ab8c77edd40dc3
NguyenAnh11 <60679961+NguyenAnh11@users.noreply.github.com>
2020-12-25 20:47:33 +0700

    design_database

---
+++ eShopSolution.Data/Configurations/ContactConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using eShopSolution.Data.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace eShopSolution.Data.Configurations
+{
+    public class ContactConfiguration : IEntityTypeConfiguration<Contact>
+    {
+        public void Configure(EntityTypeBuilder<Contact> builder)
+        {
+            builder.ToTable("Contacts");
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).UseIdentityColumn<int>();
+            builder.Property(x => x.Message).HasMaxLength(200).IsRequired();
+            builder.Property(x => x.Email).HasMaxLength(200).IsRequired();
+            builder.Property(x => x.PhoneNumber).HasMaxLength(10).IsRequired();
+            builder.Property(x => x.Message).IsRequired();
+        }
+    }
+}
---
+++ eShopSolution.Data/EF/eShopDBContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using eShopSolution.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace eShopSolution.Data.EF
+{
+    public class eShopDBContext:DbContext
+    {
+        public eShopDBContext(DbContextOptions<eShopDBContext> options)
+            : base(options) { }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
